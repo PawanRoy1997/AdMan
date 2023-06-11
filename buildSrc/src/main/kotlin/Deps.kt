d196b269e618815297f37b41ccf995898e542215
Pawan Roy <pawanroy0066@gmail.com>
2023-06-11 21:02:31 +0530

    Firebase integrated

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -9,11 +9,18 @@ object Deps {
         val list = listOf(androidCore, appCompat, constraintLayout, material)
     }
 
+    object Firebase {
+        val BOM = "com.google.firebase:firebase-bom:32.1.0"
+        val analytics = "com.google.firebase:firebase-analytics-ktx"
+    }
+
     object UnitTesting {
         val junit = "junit:junit:4.13.2"
     }
 
     object Instrumentation {
         val junit = "androidx.test.ext:junit:1.1.5"
+        val espresso = "androidx.test.espresso:espresso-core:3.5.1"
+        val list = listOf(junit, espresso)
     }
 }
